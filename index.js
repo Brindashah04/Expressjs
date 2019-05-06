3b14f755dc83a7df574030db5254746655ab703e
Brindashah04 <shahbm0409@gmail.com>
2019-05-05 22:52:22 -0400

    commit 2

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
+const logger = require('./middleware/logger');
+
 const app = express();
+
+//Init middleware
+app.use(logger);
+
+//Get All Memebers
+app.get('/api/members/:id', (req, res) => {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+
+});
+
+//Get Single Member
+app.get('/api/members', (req, res) => {
+    req.json(members);
+});
 
 const PORT = process.env.PORT || 3000;
 
